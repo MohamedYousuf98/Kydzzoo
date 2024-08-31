aa2239ccb969df0f7f04e93dd83cb208e29cd275
Mohamed Yousuf <mohamed.a.youssif@gmail.com>
2024-08-31 12:59:07 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+  // Loader
+  window.addEventListener("load", function () {
+    // Hide loader
+    document.getElementById("loader").style.display = "none";
+    // Show content
+    document.querySelector(".content").style.display = "block";
+  });
+
+
+   // Back to Top
+  let backtotop = document.querySelector(".back-to-top");
+if (backtotop) {
+  const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+      backtotop.classList.add("active");
+    } else {
+      backtotop.classList.remove("active");
+    }
+  };
+
+  // Show/hide the back to top button based on scroll
+  window.addEventListener("load", toggleBacktotop);
+  window.addEventListener("scroll", toggleBacktotop);
+
+  // Add smooth scroll to top behavior
+  backtotop.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+}
+
+  
+
 function toggleMenu() {
   const mobileMenu = document.querySelector(".mobile-menu");
   mobileMenu.classList.toggle("show");
